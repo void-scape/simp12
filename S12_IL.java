5911736fd17aff94d56bdede15e1543232f3b74b
jwstringham <jamesstringham@u.boisestate.edu>
2025-09-16 15:30:08 -0600

    chore: added necessary file

---
+++ S12_IL.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ * 
+ * @author James Stingham
+ */
+
+public class S12_IL implements S12_IL_Interface{
+
+    @Override
+    public boolean intializeMem(String filename) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'intializeMem'");
+    }
+
+    @Override
+    public String[] getProcessorState() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getProcessorState'");
+    }
+
+    @Override
+    public String getMemState() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMemState'");
+    }
+
+    @Override
+    public String update() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public boolean writeMem(String filename) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeMem'");
+    }
+
+    @Override
+    public boolean writeTrace(String filename) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeTrace'");
+    }
+    
+}
